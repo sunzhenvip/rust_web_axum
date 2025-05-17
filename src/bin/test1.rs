d087fb782824217e38b51aaa7723124e2eca26e4
sunzhen <1293334778@qq.com>
2025-05-17 15:35:19 +0800

    测试http

---
+++ src/bin/test1.rs
@@ -0,0 +1,16 @@
+#![allow(unused_imports)] // 作用于整个文件 消除没有使用的导入警告
+#![allow(dead_code)] // 消除 未使用的类型/函数/枚举变体 警告
+#![allow(unused_variables)] // 消除 未使用的变量/参数
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    // 网页请求访问 http://localhost:3000/home
+    let app:Router = Router::new().route("/home", get(|| async { "Hello, world!" }));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
