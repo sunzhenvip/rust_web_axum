4857c5fd46607deeccc0fa88e7f6fc4ff2ae6131
test <test@qq.com>
2025-05-21 17:53:00 +0800

    src/lib.rs 调整

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,17 @@ use crate::config::config_init;
 use crate::model::db_conn_init;
 use crate::router::start_route;
 
-pub mod middleware;
-pub mod router;
-pub mod handler;
-pub mod service;
-pub mod model;
 pub mod config;
-pub mod utils;
+// cargo expand --lib entities::wb_user > expanded.rs  生成的派生宏代码还源成源码 保存到 expanded.rs 文件中
 pub mod entities;
+pub mod handler;
+pub mod middleware;
+pub mod model;
+pub mod router;
+pub mod service;
+pub mod utils;
 
 pub async fn run() {
-
     //初始化
     config_init().await;
 
@@ -22,3 +22,33 @@ pub async fn run() {
     //开启路由
     start_route().await;
 }
+
+#[cfg(test)]
+mod test_expand {
+    use super::entities::wb_user;
+
+    #[test]
+    fn dummy() {
+        let _ = wb_user::Model {
+            uid: 1,
+            phone: "".to_string(),
+            password: "".to_string(),
+            created_time: 0,
+            updated_time: 0,
+        };
+    }
+}
+
+
+// mod test_expand 这个代码可以执行测试
+
+// 我在当前项目目录下运行了 cargo expand --lib entities::wb_user > expanded.rs 但是 expanded.rs 文件 IDE提示 缺少模块声明。这可能会影响智能编辑功能和自动补全。
+// 文件在 /data/network/rust/rust_web_axum/expanded.rs 位置
+// 怎么能让 rustrover IDE 能正常提示?
+
+// expanded.rs 文件ide正常提示
+// cargo expand --lib entities::wb_user > src/expanded.rs
+/*#[cfg(debug_assertions)]
+#[path = "expanded.rs"]
+mod expanded;
+*/
