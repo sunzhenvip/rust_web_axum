c320faacfd665c930faf7cf885368577564577f3
sunzhen <1293334778@qq.com>
2025-06-01 21:16:28 +0800

    std::mem::transmute 用法

--- src/bin/test3.rs
+++ src/bin/test3.rs
@@ -1,18 +1,36 @@
-fn main() {
-    let a1 = "123";
-    let a2 = "456";
-    let b3 = test1(a1, a2);
-    println!("{}", b3);
+#![allow(unused_imports)] // 作用于整个文件 消除没有使用的导入警告
+#![allow(dead_code)] // 消除 未使用的类型/函数/枚举变体 警告
+#![allow(unused_variables)] // 消除 未使用的变量/参数
+#[derive(Debug, Clone, Copy)]
+struct MyStruct {
+    age: i32,
+    num: f64,
+    // name: String,
+    name: &'static str,
 }
 
-fn test1<'a, 'b, 'out>(s1: &'a str, s2: &'b str) -> &'a str
-where
-    'a: 'b,
-    'b: 'a,
-{
-    if s1 != s2 {
-        return s1;
-    } else {
-        return s2;
+impl MyStruct {
+    fn to_bytes(&self) -> [u8; std::mem::size_of::<MyStruct>()] {
+        unsafe { std::mem::transmute(*self) }
     }
+
+    fn from_bytes(bytes: [u8; std::mem::size_of::<MyStruct>()]) -> MyStruct {
+        unsafe { std::mem::transmute(bytes) }
+    }
+}
+
+fn main() {
+    let ff = String::from("Rust");
+    let my_data = MyStruct {
+        age: 42,
+        num: 3.14,
+        name: "Rust",
+    };
+
+    let bytes = my_data.to_bytes();
+
+    println!("bytes: {:?}", bytes);
+
+    let my = MyStruct::from_bytes(bytes);
+    println!("my struct: {:?}", my);
 }
